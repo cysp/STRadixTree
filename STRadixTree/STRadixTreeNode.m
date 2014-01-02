f4922f3e5c83f1e0a4ea038438428986173fb4b1
Scott Talbot <s@chikachow.org>
2014-01-02 23:40:38 +1100

    Introduce embedded c-arrays of child nodes

--- STRadixTree/STRadixTreeNode.m
+++ STRadixTree/STRadixTreeNode.m
@@ -7,9 +7,19 @@
 #include "STRadixTreeNode.h"
 
 
+enum STRadixTreeNodePersonality {
+    STRadixTreeNodePersonalityTiny = 0,
+    STRadixTreeNodePersonalityNormal = 1,
+};
+
+static NSUInteger const STRadixTreeNodePersonalityTinyMaxChildren = 8;
+
 @implementation STRadixTreeNode {
 @private
-    NSMutableDictionary *_children;
+    enum STRadixTreeNodePersonality _personality;
+    unichar _tinyChildKeys[STRadixTreeNodePersonalityTinyMaxChildren];
+    STRadixTreeNode *_tinyChildren[STRadixTreeNodePersonalityTinyMaxChildren];
+    NSMutableDictionary *_normalChildren;
     NSMutableSet *_objects;
 }
 
@@ -19,35 +29,137 @@
 - (id)initWithKey:(NSString *)key {
     if ((self = [super init])) {
         _key = [key copy];
-        _children = [[NSMutableDictionary alloc] init];
         _objects = [[NSMutableSet alloc] init];
     }
     return self;
 }
 
-- (void)addChild:(STRadixTreeNode *)node {
-    NSParameterAssert(node.key.length);
-    NSString * const nodeKey = node.key;
-    unichar const nodeKeyFirstCharacter = [nodeKey characterAtIndex:0];
-    _children[@(nodeKeyFirstCharacter)] = node;
+- (void)st_transitionToPersonalityNormal {
+    NSAssert(_personality == STRadixTreeNodePersonalityTiny, @"");
+    NSUInteger const childrenCount = self.childrenCount;
+    NSNumber *keys[childrenCount];
+    for (NSUInteger i = 0; i < childrenCount; ++i) {
+        keys[i] = @(_tinyChildKeys[i]);
+    }
+    _normalChildren = [[NSMutableDictionary alloc] initWithObjects:_tinyChildren forKeys:keys count:childrenCount];
+    _personality = STRadixTreeNodePersonalityNormal;
 }
 
-- (void)removeChild:(STRadixTreeNode *)node {
+- (void)addChild:(STRadixTreeNode *)node {
+    NSParameterAssert(node.key.length);
+    switch (_personality) {
+        case STRadixTreeNodePersonalityTiny:
+            return [self st_tiny_addChild:node];
+        case STRadixTreeNodePersonalityNormal:
+            return [self st_normal_addChild:node];
+    }
+}
+- (void)st_tiny_addChild:(STRadixTreeNode *)node {
+    NSUInteger const childrenCount = self.childrenCount;
+    if (childrenCount == STRadixTreeNodePersonalityTinyMaxChildren) {
+        [self st_transitionToPersonalityNormal];
+        [self st_normal_addChild:node];
+        return;
+    }
     NSString * const nodeKey = node.key;
     unichar const nodeKeyFirstCharacter = [nodeKey characterAtIndex:0];
-    STRadixTreeNode * const found = _children[@(nodeKeyFirstCharacter)];
+    _tinyChildKeys[childrenCount] = nodeKeyFirstCharacter;
+    _tinyChildren[childrenCount] = node;
+}
+- (void)st_normal_addChild:(STRadixTreeNode *)node {
+    NSString * const nodeKey = node.key;
+    unichar const nodeKeyFirstCharacter = [nodeKey characterAtIndex:0];
+    _normalChildren[@(nodeKeyFirstCharacter)] = node;
+}
+
+
+- (void)removeChild:(STRadixTreeNode *)node {
+    switch (_personality) {
+        case STRadixTreeNodePersonalityTiny:
+            return [self st_tiny_removeChild:node];
+        case STRadixTreeNodePersonalityNormal:
+            return [self st_normal_removeChild:node];
+    }
+}
+- (void)st_tiny_removeChild:(STRadixTreeNode *)node {
+    NSString * const nodeKey = node.key;
+    unichar const nodeKeyFirstCharacter = [nodeKey characterAtIndex:0];
+    BOOL found = NO;
+    NSUInteger foundIndex;
+    for (NSUInteger i = 0; i < STRadixTreeNodePersonalityTinyMaxChildren; ++i) {
+        if (_tinyChildren[i] == nil) {
+            if (i > 0 && found) {
+                NSUInteger const lastIndex = i - 1;
+                _tinyChildKeys[foundIndex] = _tinyChildKeys[lastIndex];
+                _tinyChildren[foundIndex] = _tinyChildren[lastIndex];
+                _tinyChildKeys[lastIndex] = '\0';
+                _tinyChildren[lastIndex] = nil;
+            }
+            return;
+        }
+        if (_tinyChildKeys[i] == nodeKeyFirstCharacter) {
+            foundIndex = i;
+            found = YES;
+            _tinyChildKeys[i] = '\0';
+            _tinyChildren[i] = nil;
+        }
+    }
+}
+- (void)st_normal_removeChild:(STRadixTreeNode *)node {
+    NSString * const nodeKey = node.key;
+    unichar const nodeKeyFirstCharacter = [nodeKey characterAtIndex:0];
+    STRadixTreeNode * const found = _normalChildren[@(nodeKeyFirstCharacter)];
     if (found == node) {
-        [_children removeObjectForKey:@(nodeKeyFirstCharacter)];
+        [_normalChildren removeObjectForKey:@(nodeKeyFirstCharacter)];
     }
 }
 
+- (NSUInteger)childrenCount {
+    switch (_personality) {
+        case STRadixTreeNodePersonalityTiny:
+            return [self st_tiny_childrenCount];
+        case STRadixTreeNodePersonalityNormal:
+            return [self st_normal_childrenCount];
+    }
+}
+- (NSUInteger)st_tiny_childrenCount {
+    NSUInteger childrenCount = 0;
+    for (unsigned int i = 0; i < STRadixTreeNodePersonalityTinyMaxChildren; ++i) {
+        if (_tinyChildren[i]) {
+            childrenCount += 1;
+        }
+    }
+    return childrenCount;
+}
+- (NSUInteger)st_normal_childrenCount {
+    return _normalChildren.count;
+}
+
 - (NSArray *)children {
-    NSMutableArray * const children = [[NSMutableArray alloc] initWithCapacity:_children.count];
-    [_children enumerateKeysAndObjectsUsingBlock:^(id key, STRadixTreeNode *child, BOOL *stop) {
+    switch (_personality) {
+        case STRadixTreeNodePersonalityTiny:
+            return [self st_tiny_children];
+        case STRadixTreeNodePersonalityNormal:
+            return [self st_normal_children];
+    }
+}
+- (NSArray *)st_tiny_children {
+    NSUInteger childrenCount = 0;
+    for (unsigned int i = 0; i < sizeof(_tinyChildren)/sizeof(_tinyChildren[0]); ++i) {
+        if (_tinyChildren[i]) {
+            childrenCount += 1;
+        }
+    }
+    return [[NSArray alloc] initWithObjects:_tinyChildren count:childrenCount];
+}
+- (NSArray *)st_normal_children {
+    NSMutableArray * const children = [[NSMutableArray alloc] initWithCapacity:_normalChildren.count];
+    [_normalChildren enumerateKeysAndObjectsUsingBlock:^(id key, STRadixTreeNode *child, BOOL *stop) {
         [children addObject:child];
     }];
     return children;
 }
+
 - (void)setChildren:(NSArray *)children {
     for (STRadixTreeNode *child in children) {
         [self addChild:child];
@@ -56,8 +168,29 @@
 
 - (STRadixTreeNode *)childMatchingPrefixOfKey:(NSString *)key {
     NSParameterAssert(key.length);
+    switch (_personality) {
+        case STRadixTreeNodePersonalityTiny:
+            return [self st_tiny_childMatchingPrefixOfKey:key];
+        case STRadixTreeNodePersonalityNormal:
+            return [self st_normal_childMatchingPrefixOfKey:key];
+    }
+}
+- (STRadixTreeNode *)st_tiny_childMatchingPrefixOfKey:(NSString *)key {
     unichar const keyFirstCharacter = [key characterAtIndex:0];
-    STRadixTreeNode * const found = _children[@(keyFirstCharacter)];
+    for (NSUInteger i = 0; i < STRadixTreeNodePersonalityTinyMaxChildren; ++i) {
+        STRadixTreeNode * const candidateChild = _tinyChildren[i];
+        if (candidateChild) {
+            return nil;
+        }
+        if (_tinyChildKeys[i] == keyFirstCharacter) {
+            return candidateChild;
+        }
+    }
+    return nil;
+}
+- (STRadixTreeNode *)st_normal_childMatchingPrefixOfKey:(NSString *)key {
+    unichar const keyFirstCharacter = [key characterAtIndex:0];
+    STRadixTreeNode * const found = _normalChildren[@(keyFirstCharacter)];
     return found;
 }
 
