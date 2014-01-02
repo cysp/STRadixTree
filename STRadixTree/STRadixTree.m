2355fcb346c9087789a1152c84fb88e9714c8434
Scott Talbot <s@chikachow.org>
2014-01-02 13:11:43 +1100

    Split STRadixTreeNode to separate file

--- STRadixTree/STRadixTree.m
+++ STRadixTree/STRadixTree.m
@@ -2,25 +2,10 @@
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-//  Copyright (c) 2013 Scott Talbot.
+//  Copyright (c) 2013-2014 Scott Talbot.
 
 #import "STRadixTree.h"
-
-
-@interface STRadixTreeNode : NSObject
-@property (nonatomic,copy,readonly) NSString *key;
-@property (nonatomic,copy,readonly) NSArray *children;
-@property (nonatomic,copy,readonly) NSSet *objects;
-@end
-@interface STRadixTreeNode ()
-- (id)initWithKey:(NSString *)key;
-- (void)addChild:(STRadixTreeNode *)node;
-- (void)removeChild:(STRadixTreeNode *)node;
-- (void)setChildren:(NSArray *)children;
-- (STRadixTreeNode *)childMatchingPrefixOfKey:(NSString *)key;
-- (void)addObject:(id)object;
-- (void)setObjects:(NSSet *)objects;
-@end
+#import "STRadixTreeNode.h"
 
 
 @implementation STRadixTree {
@@ -118,7 +103,7 @@
     STRadixTreeNode *node = _root;
     NSMutableString *remainingKey = key.mutableCopy;
 
-    STRadixTreeNode *parentNode = node;
+    STRadixTreeNode *parentNode;
 lookupAgain:;
     {
         STRadixTreeNode *child = [node childMatchingPrefixOfKey:remainingKey];
@@ -219,85 +204,6 @@ lookupAgain:;
         *unambiguousCompletion = [child.key substringFromIndex:remainingKey.length];
     }
     return child;
-}
-
-@end
-
-
-static NSComparator const STRadixTreeNodeKeyComparator = ^(STRadixTreeNode *a, STRadixTreeNode *b) {
-    return [a.key compare:b.key];
-};
-static NSComparator const STRadixTreeNodeKeyFirstCharacterComparator = ^(NSString *a, NSString *b) {
-    if ([a isKindOfClass:[STRadixTreeNode class]]) {
-        a = ((STRadixTreeNode *)a).key;
-    }
-    if ([b isKindOfClass:[STRadixTreeNode class]]) {
-        b = ((STRadixTreeNode *)b).key;
-    }
-    unichar aFirstCharacter = 0;
-    if (a.length) {
-        aFirstCharacter = [a characterAtIndex:0];
-    }
-    unichar bFirstCharacter = 0;
-    if (b.length) {
-        bFirstCharacter = [b characterAtIndex:0];
-    }
-    if (aFirstCharacter > bFirstCharacter) {
-        return NSOrderedDescending;
-    }
-    if (aFirstCharacter < bFirstCharacter) {
-        return NSOrderedAscending;
-    }
-    return NSOrderedSame;
-};
-
-@implementation STRadixTreeNode {
-@private
-    NSMutableArray *_children;
-    NSMutableSet *_objects;
-}
-
-- (id)init {
-    return [self doesNotRecognizeSelector:_cmd], nil;
-}
-- (id)initWithKey:(NSString *)key {
-    if ((self = [super init])) {
-        _key = [key copy];
-        _children = [[NSMutableArray alloc] init];
-        _objects = [[NSMutableSet alloc] init];
-    }
-    return self;
-}
-
-- (void)addChild:(STRadixTreeNode *)node {
-    NSParameterAssert(node.key.length);
-    NSUInteger const insertionIndex = [_children indexOfObject:node inSortedRange:(NSRange){ .length = _children.count } options:NSBinarySearchingInsertionIndex usingComparator:STRadixTreeNodeKeyComparator];
-    [_children insertObject:node atIndex:insertionIndex];
-}
-
-- (void)removeChild:(STRadixTreeNode *)node {
-    [_children removeObject:node];
-}
-
-- (void)setChildren:(NSArray *)children {
-    [_children setArray:children];
-}
-
-- (STRadixTreeNode *)childMatchingPrefixOfKey:(NSString *)key {
-    NSParameterAssert(key.length);
-    NSUInteger const index = [_children indexOfObject:key inSortedRange:(NSRange){ .length = _children.count } options:0 usingComparator:STRadixTreeNodeKeyFirstCharacterComparator];
-    if (index != NSNotFound) {
-        return _children[index];
-    }
-    return nil;
-}
-
-- (void)addObject:(id)object {
-    [_objects addObject:object];
-}
-
-- (void)setObjects:(NSSet *)objects {
-    [_objects setSet:objects];
 }
 
 @end
---
+++ STRadixTree/STRadixTreeNode.h
@@ -0,0 +1,24 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//  Copyright (c) 2014 Scott Talbot. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+
+@interface STRadixTreeNode : NSObject
+@property (nonatomic,copy,readonly) NSString *key;
+@property (nonatomic,copy,readonly) NSArray *children;
+@property (nonatomic,copy,readonly) NSSet *objects;
+@end
+
+@interface STRadixTreeNode ()
+- (id)initWithKey:(NSString *)key;
+- (void)addChild:(STRadixTreeNode *)node;
+- (void)removeChild:(STRadixTreeNode *)node;
+- (void)setChildren:(NSArray *)children;
+- (STRadixTreeNode *)childMatchingPrefixOfKey:(NSString *)key;
+- (void)addObject:(id)object;
+- (void)setObjects:(NSSet *)objects;
+@end
